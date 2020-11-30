aee1f4bc74209a6c85836e1ee1ec995d1a0c312c
Alexander Schilling <dignityinside@protonmail.com>
2020-11-30 23:47:47 +0100

    Add social icons

--- views/layouts/partials/sidebar/social.php
+++ views/layouts/partials/sidebar/social.php
@@ -1,30 +1,20 @@
-<div class="widget">
-    <div class="widget-title">
-        <?= \Yii::t('app/blog', 'social_networking'); ?>
-    </div>
-    <div class="widget-content">
-        <div class="widget-content__social-icons">
-            <?php
-                $social = \Yii::$app->params['social'];
-            ?>
-            <p>
-                <a href="https://instagram.com/<?= $social['instagram'] ?>" target="_blank" rel="nofollow" title="Instagram">
-                    <i class="fab fa-instagram"></i>
-                </a>
-                <span>
-                    <a href="https://instagram.com/<?= $social['instagram'] ?>" target="_blank" rel="nofollow" title="Instagram">
-                        Instagram
+<?php if (!empty(\Yii::$app->params['social'])) : ?>
+    <div class="widget">
+        <div class="widget-title">
+            <?= \Yii::t('app/blog', 'social_networking'); ?>
+        </div>
+        <div class="widget-content">
+            <div class="social_buttons">
+                <?php foreach (\Yii::$app->params['social'] as $key => $value) : ?>
+                    <a href="<?= $value['url'] ?>" target="_blank" rel="nofollow noopener"
+                       style="text-decoration: none" title="<?= $value['title'] ?>">
+                        <div class="social_buttons__item" style="background: <?= $value['bgColor'] ?>">
+                            <i class="<?= $value['faClass']; ?>"></i>
+                        </div>
                     </a>
-                </span>
-            </p>
-            <p>
-                <a href="https://t.me/<?= $social['telegram'] ?>" target="_blank" rel="nofollow" title="Telegram канал"><i class="fa fa-telegram"></i></a>
-                <span>
-                    <a href="https://t.me/<?= $social['telegram'] ?>" target="_blank" rel="nofollow" title="Telegram канал">
-                        Telegram
-                    </a>
-                </span>
-            </p>
+                <?php endforeach; ?>
+            </div>
         </div>
     </div>
-</div>
+<?php endif; ?>
+
